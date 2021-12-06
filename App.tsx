1533b9af6d3abbd1d4ea7c5081f65a09df1b58a9
QingOu <qingqingou@gmail.com>
2021-12-05 22:43:29 -0500

    add check winner logic

--- App.tsx
+++ App.tsx
@@ -1,59 +1,153 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, Pressable } from 'react-native';
-const bg = require('./assets/bg.jpeg') ;
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, ImageBackground, Pressable, Alert } from "react-native";
+const bg = require("./assets/bg.jpeg");
 
+const emptyMap = [
+  ["", "", ""], //1st row
+  ["", "", ""], //2nd row
+  ["", "", ""], //3rd row
+];
 export default function App() {
-  const [map, setMap] = useState([
-    ['', '', ''], //1st row
-    ['', '', ''], //2nd row
-    ['', '', ''] //3rd row
-    
-  ])
+  const [map, setMap] = useState(emptyMap);
 
-  const [currentTurn, setCurrentTurn] = useState('x')
+  const [currentTurn, setCurrentTurn] = useState("x");
 
   const onPress = (rowIndex: number, columnIndex: number) => {
-    console.warn('hello', rowIndex, columnIndex)
+    //console.warn("hello", rowIndex, columnIndex);
 
-    if(map[rowIndex][columnIndex] !== ''){
-      alert('Position already occupied');
+    if (map[rowIndex][columnIndex] !== "") {
+      alert("Position already occupied");
       return;
     }
 
     setMap((existingMap) => {
-      const updatedMap =[...existingMap]
+      const updatedMap = [...existingMap];
       updatedMap[rowIndex][columnIndex] = currentTurn;
       return updatedMap;
-      
     });
 
-    setCurrentTurn(currentTurn === 'x' ? 'o' : 'x');
-  }
+    setCurrentTurn(currentTurn === "x" ? "o" : "x");
+    checkWinningState();
+    checkTieState();
+  };
+
+  const checkWinningState = () => {
+    //check rows
+    for (let i = 0; i < 3; i++) {
+      const isRowXWinning = map[i].every((cell) => cell === "x");
+      const isRowOWinning = map[i].every((cell) => cell === "o");
+
+      if (isRowXWinning) {
+        gameWon("x");
+      }
+
+      if (isRowOWinning) {
+        gameWon("o");
+      }
+    }
+
+    //check columns
+    for (let col = 0; col < 3; col++) {
+      let isColumnXWinner = true;
+      let isColumnOWinner = true;
+
+      for (let row = 0; row < 3; row++) {
+        if (map[row][col] !== "x") {
+          isColumnXWinner = false;
+        }
+        if (map[row][col] !== "o") {
+          isColumnOWinner = false;
+        }
+      }
+
+      if (isColumnXWinner) {
+        gameWon("x");
+        break;
+      }
+
+      if (isColumnOWinner) {
+        gameWon("o");
+        break;
+      }
+    }
+
+    //check diagnoals
+    let isDiagonal1OWinning = true;
+    let isDiagonal1XWinning = true;
+    let isDiagonal2OWinning = true;
+    let isDiagonal2XWinning = true;
+    for (let i = 0; i < 3; i++) {
+      if (map[i][i] !== "o") {
+        isDiagonal1OWinning = false;
+      }
+      if (map[i][i] !== "x") {
+        isDiagonal1XWinning = false;
+      }
+      if (map[i][2 - i] !== "o") {
+        isDiagonal2OWinning = false;
+      }
+      if (map[i][2 - i] !== "x") {
+        isDiagonal2XWinning = false;
+      }
+    }
+
+    if (isDiagonal1OWinning || isDiagonal2OWinning) {
+      gameWon("o");
+    }
+    if (isDiagonal1XWinning || isDiagonal2XWinning) {
+      gameWon("x");
+    }
+  };
+
+  const checkTieState = () => {
+    if (!map.some((row) => row.some((cell) => cell === ""))) {
+      Alert.alert(`Its a tie`, `tie`, [
+        {
+          text: "Restart",
+          onPress: resetGame,
+        },
+      ]);
+    }
+  };
+  const gameWon = (player: string) => {
+    Alert.alert(`Hurray`, `Player ${player} won`, [
+      {
+        text: "Restart",
+        onPress: resetGame,
+      },
+    ]);
+  };
+  const resetGame = () => {
+    setMap([
+      ["", "", ""], //1st row
+      ["", "", ""], //2nd row
+      ["", "", ""], //3rd row
+    ]);
+    setCurrentTurn("x");
+  };
   return (
     <View style={styles.container}>
-      <ImageBackground source={bg} style={styles.bg} resizeMode='contain'>
-        
+      <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
         <View style={styles.map}>
           {map.map((row, rowIndex) => (
-            <View key={rowIndex} style={styles.row}>
-               {row.map((cell, columnIndex) => (
-                <Pressable key={columnIndex} onPress={() => onPress(rowIndex, columnIndex)} style={styles.cell}>
-                   {cell === 'o' && <View style={styles.circle} />}
-                   {cell === 'x' && (
-                      <View style={styles.cross}>
-                        <View style={styles.crossLine} />
-                        <View style={[styles.crossLine, styles.crossLineReversed]} />
-                      </View>
-                      )}
-                 </Pressable>
-                ))}
+            <View key={`row-${rowIndex}`} style={styles.row}>
+              {row.map((cell, columnIndex) => (
+                <Pressable key={`row-${rowIndex}-col-${columnIndex}`} onPress={() => onPress(rowIndex, columnIndex)} style={styles.cell}>
+                  {cell === "o" && <View style={styles.circle} />}
+                  {cell === "x" && (
+                    <View style={styles.cross}>
+                      <View style={styles.crossLine} />
+                      <View style={[styles.crossLine, styles.crossLineReversed]} />
+                    </View>
+                  )}
+                </Pressable>
+              ))}
             </View>
           ))}
         </View>
-
       </ImageBackground>
-      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -62,69 +156,65 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#242034'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#242034",
   },
 
-  bg:{
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 15
+  bg: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 15,
   },
 
-  map:{
-    width: '80%',
+  map: {
+    width: "80%",
     aspectRatio: 1,
-
   },
-  row:{
+  row: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
-  cell:{
+  cell: {
     width: 100,
     height: 100,
-    flex:1,
+    flex: 1,
   },
 
-  circle:{
+  circle: {
     flex: 1,
     width: 75,
     height: 75,
     borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     margin: 10,
     borderWidth: 10,
-    borderColor: 'white'
+    borderColor: "white",
   },
-  cross:{
-    flex: 1
+  cross: {
+    flex: 1,
   },
-  crossLine:{
-    position: 'absolute',
-    left: '50%',
+  crossLine: {
+    position: "absolute",
+    left: "50%",
     width: 10,
-    height: '100%',
-    backgroundColor: 'white',
+    height: "100%",
+    backgroundColor: "white",
     borderRadius: 5,
-    transform:[
-      {
-        rotate: '45deg'
-      },
-
-    ]
-  },
-  crossLineReversed:{
     transform: [
       {
-        rotate: '-45deg',
+        rotate: "45deg",
       },
-
-    ]
-
-  }
+    ],
+  },
+  crossLineReversed: {
+    transform: [
+      {
+        rotate: "-45deg",
+      },
+    ],
+  },
 });
