4eef10d89bbae7c1c8c03753f90cb92a9eb236db
QingOu <qingqingou@gmail.com>
2021-12-30 17:17:08 -0500

    create bot to play with you

--- App.tsx
+++ App.tsx
@@ -11,9 +11,14 @@ const emptyMap = [
   ["", "", ""], //2nd row
   ["", "", ""], //3rd row
 ];
+
+const copyArray = (original: string[][]) => {
+  const copy = JSON.parse(JSON.stringify(original));
+  return copy;
+};
+
 export default function App() {
   const [map, setMap] = useState(emptyMap);
-
   const [currentTurn, setCurrentTurn] = useState("x");
 
   useEffect(() => {
@@ -21,6 +26,16 @@ export default function App() {
       botTurn();
     }
   }, [currentTurn]);
+
+  useEffect(() => {
+    const winner = getWinner(map);
+    if (winner) {
+      gameWon(winner);
+    } else {
+      checkTieState();
+    }
+  }, [map]);
+
   const onPress = (rowIndex: number, columnIndex: number) => {
     //console.warn("hello", rowIndex, columnIndex);
 
@@ -36,19 +51,13 @@ export default function App() {
     });
 
     setCurrentTurn(currentTurn === "x" ? "o" : "x");
-    const winner = getWinner();
-    if (winner) {
-      gameWon(winner);
-    } else {
-      checkTieState();
-    }
   };
 
-  const getWinner = () => {
+  const getWinner = (winnerMap: string[][]) => {
     //check rows
     for (let i = 0; i < 3; i++) {
-      const isRowXWinning = map[i].every((cell) => cell === "x");
-      const isRowOWinning = map[i].every((cell) => cell === "o");
+      const isRowXWinning = winnerMap[i].every((cell) => cell === "x");
+      const isRowOWinning = winnerMap[i].every((cell) => cell === "o");
 
       if (isRowXWinning) {
         return "x";
@@ -65,10 +74,10 @@ export default function App() {
       let isColumnOWinner = true;
 
       for (let row = 0; row < 3; row++) {
-        if (map[row][col] !== "x") {
+        if (winnerMap[row][col] !== "x") {
           isColumnXWinner = false;
         }
-        if (map[row][col] !== "o") {
+        if (winnerMap[row][col] !== "o") {
           isColumnOWinner = false;
         }
       }
@@ -90,16 +99,16 @@ export default function App() {
     let isDiagonal2OWinning = true;
     let isDiagonal2XWinning = true;
     for (let i = 0; i < 3; i++) {
-      if (map[i][i] !== "o") {
+      if (winnerMap[i][i] !== "o") {
         isDiagonal1OWinning = false;
       }
-      if (map[i][i] !== "x") {
+      if (winnerMap[i][i] !== "x") {
         isDiagonal1XWinning = false;
       }
-      if (map[i][2 - i] !== "o") {
+      if (winnerMap[i][2 - i] !== "o") {
         isDiagonal2OWinning = false;
       }
-      if (map[i][2 - i] !== "x") {
+      if (winnerMap[i][2 - i] !== "x") {
         isDiagonal2XWinning = false;
       }
     }
@@ -146,6 +155,7 @@ export default function App() {
   const botTurn = () => {
     //collect all possible options
     const possiblePositions: position[] = [];
+
     map.forEach((row, rowIndex) => {
       row.forEach((cell, columnIndex) => {
         if (cell === "") {
@@ -153,21 +163,71 @@ export default function App() {
         }
       });
     });
-    //choose the best option
-    const chosenOption = possiblePositions[Math.floor(Math.random() * possiblePositions.length)];
+
+    let chosenOption;
+
+    //Attack
+    possiblePositions.forEach((possiblePosition) => {
+      const mapCopy = copyArray(map);
+      mapCopy[possiblePosition.row][possiblePosition.col] = "o";
+
+      const winner = getWinner(mapCopy);
+
+      if (winner === "o") {
+        //attack that position
+        chosenOption = possiblePosition;
+      }
+    });
+
+    if (!chosenOption) {
+      //Defend
+      //Check if the opponent WINS if it takes one of the possible positions
+      possiblePositions.forEach((possiblePosition) => {
+        const mapCopy = copyArray(map);
+        mapCopy[possiblePosition.row][possiblePosition.col] = "x";
+
+        const winner = getWinner(mapCopy);
+
+        if (winner === "x") {
+          //Defend that position
+          chosenOption = possiblePosition;
+        }
+      });
+    }
+
+    //choose random
+    if (!chosenOption) {
+      chosenOption =
+        possiblePositions[Math.floor(Math.random() * possiblePositions.length)];
+    }
+
     if (chosenOption) {
       onPress(chosenOption.row, chosenOption.col);
     }
   };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
-        <Text style={{ fontSize: 24, color: "white", position: "absolute", top: 50 }}>Current Turn: {currentTurn.toUpperCase()}</Text>
+        <Text
+          style={{
+            fontSize: 24,
+            color: "white",
+            position: "absolute",
+            top: 50,
+          }}
+        >
+          Current Turn: {currentTurn.toUpperCase()}
+        </Text>
         <View style={styles.map}>
           {map.map((row, rowIndex) => (
             <View key={`row-${rowIndex}`} style={styles.row}>
               {row.map((cell, columnIndex) => (
-                <Cell key={`row-${rowIndex}-col-${columnIndex}`} cell={cell} onPress={() => onPress(rowIndex, columnIndex)} />
+                <Cell
+                  key={`row-${rowIndex}-col-${columnIndex}`}
+                  cell={cell}
+                  onPress={() => onPress(rowIndex, columnIndex)}
+                />
               ))}
             </View>
           ))}
